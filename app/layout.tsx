dce3b529f5ed68e9f2f48b01f2772a898277c432
홍길동 <your-email@example.com>
2025-07-23 09:08:45 +0900

    fix: Channel Talk 스크립트를 layout.tsx로 이동하여 안정성 개선
    - Channel Talk를 Footer에서 layout.tsx로 이동
    - 스크립트 로딩 전략을 lazyOnload로 변경
    - 페이지 로딩 순서 최적화로 Channel Talk 안정성 향상

--- app/layout.tsx
+++ app/layout.tsx
@@ -130,6 +130,21 @@ export default function RootLayout({
           src="https://js.hs-scripts.com/24900000.js"
         />
         
+        {/* Channel Talk */}
+        <Script
+          id="channel-talk"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+
+              ChannelIO('boot', {
+                "pluginKey": "4c0cca0c-7cf1-4441-8f11-3e04995a4a78"
+              });
+            `,
+          }}
+        />
+        
         {/* 추가 SEO 메타 태그 */}
         <meta name="format-detection" content="telephone=yes" />
         <meta name="mobile-web-app-capable" content="yes" />
--- components/footer.tsx
+++ components/footer.tsx
@@ -15,21 +15,6 @@ export function Footer() {
         src="//js.hs-scripts.com/43932435.js"
       />
       
-      {/* Channel Talk */}
-      <Script
-        id="channel-talk"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
-
-            ChannelIO('boot', {
-              "pluginKey": "4c0cca0c-7cf1-4441-8f11-3e04995a4a78"
-            });
-          `,
-        }}
-      />
-      
       {/* Cal.com Floating Popup */}
       <Script
         id="cal-floating-popup"
